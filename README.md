91a8b10c6b12f0ed7f06b89228729f0b3c57064d
Kim Seong Jung <icebreaker70@gmail.com>
2024-09-18 12:35:39 +0900

    초기 코드 작성

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # kans-calico
 KANS - 3주차 (Vanilla k8s + calico cni)
+
+# Provision
+$ vagrant up --provision
---
+++ scripts/route2.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#set -x
+
+IP=$(ip -br -4 addr | grep eth1 | awk '{print $3}')
+
+cat <<EOT> /etc/netplan/50-vagrant.yaml
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    eth1:
+      addresses:
+      - $IP
+      routes:
+      - to: 192.168.10.0/24
+        via: 192.168.20.254
+EOT
+
+chmod 600 /etc/netplan/00-installer-config.yaml
+chmod 600 /etc/netplan/01-netcfg.yaml
+chmod 600 /etc/netplan/50-vagrant.yaml
+netplan apply
